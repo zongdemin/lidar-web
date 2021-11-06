312dd5797c80651d790704ff6b3d95d905b1647d
zongdemin <zongdemin@126.com>
2021-11-06 19:47:15 +0800

    添加样式

--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -1,41 +1,26 @@
 <template>
-  <div class="map-box">
-    <div id="cesiumContainer"></div>
+  <div id="cesiumContainer">
   </div>
 </template>
 
 <script>
 import * as Cesium from 'cesium/Source/Cesium.js'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
 export default {
   name: '',
   mounted () {
    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3OTFiOTBiZC0zOGQwLTQyZmItYjFlNy0xMTgxZGE4OGZmNjkiLCJpZCI6NDYyNjksImlhdCI6MTYxNTk5MDQwM30.Gn73KKubCqQT2ONhNGUb3iHnom9nHMqorMTvRC6z7hI'
-   var viewer = Cesium.Viewer('cesiumContainer', {
-            geocoder: true,
-            sceneModePicker: true,
-            navigationHelpButton: true,
-            baseLayerPicker: true,
-            homeButton: true,
-            fullscreenButton: true,
-            timeline: true,
-            animation: true })
+   Cesium.Viewer('cesiumContainer')
     // 谷歌影像
-    viewer._cesiumWidget._creditContainer.style.display = 'none'
-    var googleImagery = new Cesium.UrlTemplateImageryProvider({
-      url: 'http://mt{s}.google.cn/vt/lyrs=s&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}',
-      subdomains: '0123'
-    })
-    viewer.imageryLayers.addImageryProvider(googleImagery)
+    // viewer._cesiumWidget._creditContainer.style.display = 'none'
+    // var googleImagery = new Cesium.UrlTemplateImageryProvider({
+    //   url: 'http://mt{s}.google.cn/vt/lyrs=s&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}',
+    //   subdomains: '0123'
+    // })
+    // viewer.dataSources.removeAll()
+    // viewer.imageryLayers.addImageryProvider(googleImagery)
   }
 }
 </script>
-<style scoped>
-.map-box {
-  width: 1024px;
-  height: 768px;
-}
-#cesiumContainer {
-  width: 100%;
-  height: 100%;
-}
+<style lang="less" scoped>
 </style>
